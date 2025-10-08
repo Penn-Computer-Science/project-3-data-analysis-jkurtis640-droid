eb5f8b46d122f1dfd2803bb5405d5261b194f63b
jkurtis640-droid <jkurtis640@phm.k12.in.us>
2025-10-08 09:49:08 -0400

    Update notpandas2.py

--- notpandas2.py
+++ notpandas2.py
@@ -1,7 +1,7 @@
 
 import pandas as pd
 import random
-import matplotlib
+import matplotlib.pyplot as plt
 ## Load our dataframe
 df = pd.read_csv('notpandas.csv')
 not_pandas_data = pd.DataFrame(df)
@@ -22,3 +22,30 @@ print(not_pandas_data['Time listening music'].value_counts())
 
 not_pandas_data_sorted = df.sort_values(by='Hours playing videogames')
 print(df)
+
+not_pandas_data.groupby('Time listening music')["Hours playing videogames"].mean().plot(kind="bar")
+plt.show()
+
+plt.hist(not_pandas_data, bins=10, edgecolor='blue')
+# Creates the histogram
+plt.ylabel("time listening to music")
+# labeling the x axis
+plt.xlabel("hours playing videogames")
+# labeling the y axis
+plt.title("Time listening to music vs Hours playing videogames")
+# Does the title
+plt.legend()
+plt.show()
+
+fig, ax = plt.subplots()
+# figure and axis allows to plot a line
+for name, group in df.groupby('Hours playing videogames'):
+# allows the ability to group the line plot seperately
+    group.plot(x='Time listening music',y='Hours playing videogames',label='name',ax=ax,kind='line')
+    # Sets the parameters of the line plot
+
+ax.set_title("hours playing videogames vs time listening music")
+ax.set_xlabel("Time listening music")
+ax.set_ylabel("Hours playing videogames")
+ax.legend()
+plt.show()
